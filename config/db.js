8984275d0cbbfcbb0b774229b879cd55a7ccf6d9
marcos2205 <marcos2205@gmail.com>
2020-04-27 19:58:51 -0300

    teste

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV == "production") {
     module.exports = {        
-        mongoURI: "mongodb+srv://sa:123local@cluster0-nuhhq.mongodb.net/test?retryWrites=true&w=majority"
+        mongoURI: "mongodb+srv://sa:123local@cluster0-nuhhq.mongodb.net/test"
     }
 } else {
     module.exports = { mongoURI: "mongodb://localhost/blogapp" }
